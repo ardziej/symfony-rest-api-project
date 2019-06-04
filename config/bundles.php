8f95eea3436844f6f951237b06dc1573f5b2eaab
Michal Ardziejewski <adjweb@gmail.com>
2019-06-04 08:36:22 +0200

    Add JWT Auth

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,11 @@
 <?php
 
 return [
-	Symfony\Bundle\FrameworkBundle\FrameworkBundle::class                => [ 'all' => true ],
-	Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => [ 'all' => true ],
-	FOS\RestBundle\FOSRestBundle::class                                  => [ 'all' => true ],
-	JMS\SerializerBundle\JMSSerializerBundle::class                      => [ 'all' => true ],
-	EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle::class       => [ 'all' => true ],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
